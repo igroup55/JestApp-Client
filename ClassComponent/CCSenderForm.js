d51c6d25b0f9e558798d6695dc467dcbdd7cdd6b
igroup55 <77381056+igroup55@users.noreply.github.com>
2021-03-22 21:19:39 +0200

    ClientSide
    22.03.2021

--- ClassComponent/CCSenderForm.js
+++ ClassComponent/CCSenderForm.js
@@ -1,6 +1,6 @@
 import React, { Component, useState } from 'react';
 import { CheckBox, Container, Header, Content, Form, Item, Input, Label, Picker, Footer, Right, Button, Icon } from 'native-base';
-import { SafeAreaView, ScrollView, StyleSheet, TextInput, Text , View } from 'react-native';
+import { SafeAreaView, ScrollView, StyleSheet, TextInput, Text, View } from 'react-native';
 import CheckBoxes from './CCCheckBox';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import ReactDOM from "react-dom";
@@ -12,37 +12,41 @@ export default class CCSenderForm extends Component {
       selected1: null,
       selected2: null,
       selected3: null,
-      StationsList : [],
-      CustPNum : ' ',
-      CustName :' ',
-      EmptyLocker : null
+      StationsList: [],
+      CustPNum: ' ',
+      CustName: ' ',
+      CustomerId: '',
+      PackageID: null,
+      SEmptyLocker: null,
+      EEmptyLocker: null
+
     };
 
   }
 
- async componentDidMount () {
-//  fetch('http://proj.ruppin.ac.il/igroup55/test2/tar1/api/Stations',{
-//       method: 'GET',
-//         headers: {
-//              Accept: 'application/json, text/json, charset=UTF-8',
-//              'Content-Type': 'application/json, text/json'
-//        }
-//       })
-//       .then(response => response.json())
-//      .then(data => {
-//        this.setState({StationsList:data});
-//      })
-//      .catch(err => console.error(err));
+  async componentDidMount() {
+    //  fetch('http://proj.ruppin.ac.il/igroup55/test2/tar1/api/Stations',{
+    //       method: 'GET',
+    //         headers: {
+    //              Accept: 'application/json, text/json, charset=UTF-8',
+    //              'Content-Type': 'application/json, text/json'
+    //        }
+    //       })
+    //       .then(response => response.json())
+    //      .then(data => {
+    //        this.setState({StationsList:data});
+    //      })
+    //      .catch(err => console.error(err));
 
-     const apiStationsUrl ='http://proj.ruppin.ac.il/igroup55/test2/tar1/api/Stations';
-     const response = await fetch(apiStationsUrl);
-     const data = await response.json()
-     console.log(data)
-     this.setState({StationsList:data})
+    const apiStationsUrl = 'http://proj.ruppin.ac.il/igroup55/test2/tar1/api/Stations';
+    const response = await fetch(apiStationsUrl);
+    const data = await response.json()
+    console.log(data)
+    this.setState({ StationsList: data })
 
 
-    
-};
+
+  };
 
   onValueChange1 = (value) => {
     this.setState({
@@ -50,7 +54,7 @@ export default class CCSenderForm extends Component {
     });
     console.log(this.state.selected1)
   }
-  
+
   onValueChange2 = (value) => {
     this.setState({
       selected2: value
@@ -63,68 +67,74 @@ export default class CCSenderForm extends Component {
   }
   onChangeText = (key, value) => {
     this.setState({ [key]: value })
-  
+
   }
 
   AddCust = () => {
-    
-    const customer_data = {
-CustomerId: '18',
-PackageID: '23353',
-      FullName : this.state.CustName,
-      PhoneNum : this.state.CustPNum
-      
- 
-     }
 
-     fetch('http://proj.ruppin.ac.il/igrop55/test2/tar1/api/Customers', {
+    const customer_data = {
+      CustomerId: '18',
+      PackageID: this.state.PackageID,
+      FullName: this.state.CustName,
+      PhoneNum: this.state.CustPNum
+
+
+    }
+
+    fetch('http://proj.ruppin.ac.il/igrop55/test2/tar1/api/Customers', {
       method: 'POST',
       body: JSON.stringify(customer_data),
       headers: new Headers({
-      'Content-type': 'application/json; charset=UTF-8' //very important to add the 'charset=UTF-8'!!!!
+        'Content-type': 'application/json; charset=UTF-8' //very important to add the 'charset=UTF-8'!!!!
       })
-      })
+    })
 
 
   }
 
-   addPack = () => {
+  async addPack() {
 
-// ----------------------------------------------------------------------------------------------------------------
-// Search For empty lockers ( לוקרים פנויים )
-// ----------------------------------------------------------------------------------------------------------------
-    
-    const apiStationsUrl ='http://proj.ruppin.ac.il/igroup55/test2/tar1/api/Lockers';
-     const response = await fetch(apiStationsUrl);
-     const data = await response.json()
-     console.log(data)
-     this.setState({EmptyLocker:data})
+    // ----------------------------------------------------------------------------------------------------------------
+    // Search For empty lockers ( לוקרים פנויים )
+    // ----------------------------------------------------------------------------------------------------------------
+    let StartStation = this.state.selected1;
+    const apiLockersUrl1 = 'http://proj.ruppin.ac.il/igroup55/test2/tar1/api/Lockers?StationID=' + StartStation;
+    const response1 = await fetch(apiLockersUrl1);
+    const Start = await response1.json()
+    this.setState({ SEmptyLocker: Start })
+    console.log(this.state.SEmptyLocker)
 
-     
+    let EndStation = this.state.selected2;
+    const apiLockersUrl = 'http://proj.ruppin.ac.il/igroup55/test2/tar1/api/Lockers?StationID=' + EndStation;
+    const response = await fetch(apiLockersUrl);
+    const End = await response.json()
 
-     if(this.state.EmptyLocker !== null)
-     {
+    this.setState({ EEmptyLocker: End })
+    console.log(this.state.EEmptyLocker)
+
+   if (this.state.EEmptyLocker !== null && this.state.SEmptyLocker !== null) {
+
+
       const package_data = {
 
         StartStation: this.state.selected1,
         EndStation: this.state.selected2,
         Pweight: this.state.selected3,
-        Status : 1
-        
-  
+        Status: 1
+
+
       }
 
       fetch('http://proj.ruppin.ac.il/igroup55/test2/tar1/api/Packages', {
         method: 'POST',
         body: JSON.stringify(package_data),
         headers: new Headers({
-        'Content-type': 'application/json; charset=UTF-8' //very important to add the 'charset=UTF-8'!!!!
+          'Content-type': 'application/json; charset=UTF-8' //very important to add the 'charset=UTF-8'!!!!
         })
-        })
-  
-        {this.AddCust()}
+      })
+      
 
-   
+      { this.AddCust() }
 
     }
 
@@ -135,60 +145,57 @@ PackageID: '23353',
         StartStation: this.state.selected1,
         EndStation: this.state.selected2,
         Pweight: this.state.selected3,
-        Status : 0
-        
-  
+        Status: 0
+
+
       }
 
       fetch('http://proj.ruppin.ac.il/igroup55/test2/tar1/api/Packages', {
         method: 'POST',
         body: JSON.stringify(package_data),
         headers: new Headers({
-        'Content-type': 'application/json; charset=UTF-8' //very important to add the 'charset=UTF-8'!!!!
+          'Content-type': 'application/json; charset=UTF-8' //very important to add the 'charset=UTF-8'!!!!
         })
-        })
-  
-        {this.AddCust()}
+      })
 
+      { this.AddCust() }
 
     }
 
-    this.props.navigation.navigate('DeliveryFeed'); 
-    
+    this.props.navigation.navigate('DeliveryFeed');
+
   }
 
   render() {
 
-    let stations = this.state.StationsList.map((stations,key)=>{
-      return ( <Picker.Item key={key} label={stations.StationName} value={stations.StationName} />)});
-    
+    let stations = this.state.StationsList.map((stations, key) => {
+      return (<Picker.Item key={key} label={stations.StationName} value={stations.StationID} />)
+    });
+
     return (
       <SafeAreaView>
         <ScrollView>
           <View >
             <Header style={{ backgroundColor: 'green', borderBottomWidth: 2, borderColor: 'black', borderBottomColor: 'black' }}><Text style={{ fontSize: 30, fontWeight: 'bold', backgroundColor: 'green' }}> JestApp</Text></Header>
 
-
             <Form style={{ width: 390 }}>
               <View >
-               
+
                 <Icon name="location" style={{ alignSelf: 'center', marginTop: 10 }} />
                 <Text style={styles.titles} >תחנת מוצא</Text>
                 <Item picker style={styles.InputText}>
                   <Picker
                     mode="dropdown"
-                    style={{  textAlign: 'right' }}
+                    style={{ textAlign: 'right' }}
                     placeholder="בחר תחנת מוצא"
                     placeholderStyle={{ color: "#bfc6ea" }}
                     placeholderIconColor="#007aff"
                     selectedValue={this.state.selected1}
                     onValueChange={this.onValueChange1.bind(this)}
                   >
-           
-           <Picker.Item label='בחר תחנת מוצא' value='None' /> 
-    
-         {stations}
-            
+
+                    <Picker.Item label='בחר תחנת מוצא' value='None' />
+                    {stations}
                   </Picker>
                 </Item>
               </View>
@@ -212,32 +219,32 @@ PackageID: '23353',
                 </Item>
               </View>
 
-              <CheckBoxes  />
+              <CheckBoxes />
               <Icon name="person" style={{ alignSelf: 'center', marginTop: 10 }} />
-                <Text style={styles.titles}> פרטי לקוח קצה </Text>
+              <Text style={styles.titles}> פרטי לקוח קצה </Text>
 
-               
-               
-                   <Item>
-          <Input style={styles.InputText}
-            placeholderTextColor="grey"
-            placeholder="שם"
-            returnKeyType="search"
-           
-            onChangeText={val => this.setState({ CustName: val })}
-          />
-        </Item>
-   <Text>{this.state.CustName}</Text>
-        <Item>
-          <Input style={styles.InputText}
-            placeholderTextColor="grey"
-            placeholder="טלפון"
-            returnKeyType="search"
-       
-            onChangeText={val => this.setState({ CustPNum: val })}
-          />
-        </Item>
-              <View  style={styles.section}>
+
+
+              <Item>
+                <Input style={styles.InputText}
+                  placeholderTextColor="grey"
+                  placeholder="שם"
+                  returnKeyType="search"
+
+                  onChangeText={val => this.setState({ CustName: val })}
+                />
+              </Item>
+              <Text>{this.state.CustName}</Text>
+              <Item>
+                <Input style={styles.InputText}
+                  placeholderTextColor="grey"
+                  placeholder="טלפון"
+                  returnKeyType="search"
+
+                  onChangeText={val => this.setState({ CustPNum: val })}
+                />
+              </Item>
+              <View style={styles.section}>
                 <Icon name="line-weight" style={{ alignSelf: 'center', marginTop: 10 }} />
                 <Text style={styles.titles}> משקל חבילה </Text>
                 <Item picker style={styles.InputText}>
@@ -267,7 +274,7 @@ PackageID: '23353',
                 </Item></View> */}
 
               <Button onPress={() => { this.addPack() }} style={{ alignSelf: 'center', backgroundColor: 'green', marginTop: 70, borderRadius: 10, borderWidth: 1, borderColor: 'black' }}><Text style={{ fontWeight: 'bold' }}>  צור כרטיס משלוח  </Text></Button>
-            
+
             </Form>
           </View>
         </ScrollView>
@@ -291,14 +298,14 @@ const styles = ({
     backgroundColor: '#cbe8ba',
     borderRadius: 10,
     marginRight: 10,
-    marginTop:10
+    marginTop: 10
 
   },
   titles: {
     textAlign: 'center',
     fontWeight: 'bold',
-marginBottom: 5,
-marginTop:5,
+    marginBottom: 5,
+    marginTop: 5,
     alignItems: 'center'
   },
   section: {
